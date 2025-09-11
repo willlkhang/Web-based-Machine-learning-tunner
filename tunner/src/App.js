3e1a670f344c825190fca6050d341631f661fa9e
willlkhang <minhkhangnguyen31104@gmail.com>
2025-09-11 13:55:38 +0800

    modify the app.js

--- tunner/src/App.js
+++ tunner/src/App.js
@@ -2,12 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, {useEffect, useState} from 'react';
-
 import io from 'socket.io-client';
+
+import DataTable from '.components/DataTable';
+import Form from '.components/Form';
+import Gear from '.components/Gear';
+import Header from '.components/Header';
+import Message from '.components/Message';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const socket = io('http://localhost:9000');
 
 function App() {
+
+  const [processData, setProgressData] = useState({
+    time: 0,
+    total_progress: 0,
+  });
+
+  const [epochs, setEpochs] = useState('');
+  const [lr, setLr] = useState('');
+  const [size, setSize] = useState('');
+  const [table, setTable] = useState('');
+  const [message, setMessage] = useState('');
+  const d = new Date();
+
   return (
     <div className="App">
       <header className="App-header">
